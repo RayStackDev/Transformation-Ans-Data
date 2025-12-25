4e8d655c9470ff4e5ebee3c80781d1d5bd16123d
RayStackDev <raymondlugo64@gmail.com>
2025-12-25 18:33:35 -0400

    Fazendo a verificaçao das paginas legiveis

--- extracao_tabela.py
+++ extracao_tabela.py
@@ -5,3 +5,8 @@ caminho_pdf = "Anexo/Anexo_I_Rol_2021RN_465.2021_RN654.2025.pdf"
 with pdfplumber.open(caminho_pdf) as pdf:
     print(f"O PDF tem {len(pdf.pages)} paginas.")
     
+    for numero_pagina, pagina in enumerate(pdf.pages, start=1):
+        print(f"Processando paginas {numero_pagina}")
+
+        tabelas = pagina.extract_tables()
+        print(f"Numeros de tabelas encontradas: {len(tabelas)}")
