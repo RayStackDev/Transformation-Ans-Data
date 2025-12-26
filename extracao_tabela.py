c6e97e1c73cfca4f404833854b61c13185b8090b
RayStackDev <raymondlugo64@gmail.com>
2025-12-25 21:21:00 -0400

    Tratando os dados do PDF, substituindo abreviações e salvando em CSV

--- extracao_tabela.py
+++ extracao_tabela.py
@@ -1,12 +1,40 @@
 import pdfplumber
+import csv
+import os
+
 
 caminho_pdf = "Anexo/Anexo_I_Rol_2021RN_465.2021_RN654.2025.pdf"
 
 with pdfplumber.open(caminho_pdf) as pdf:
     print(f"O PDF tem {len(pdf.pages)} paginas.")
     
+    todas_linhas = []
+
     for numero_pagina, pagina in enumerate(pdf.pages, start=1):
-        print(f"Processando paginas {numero_pagina}")
 
         tabelas = pagina.extract_tables()
-        print(f"Numeros de tabelas encontradas: {len(tabelas)}")
+    
+
+        for tabela in tabelas:
+            for linha in tabela[1:]:
+                todas_linhas.append(linha)
+
+substituicoes = {
+    "OD": "Odontologica",
+    "AMD": "Ambulatorial"
+}
+
+for linha in todas_linhas:
+    if linha[2] in substituicoes:
+        linha[2] = substituicoes[linha[2]]
+
+caminho_csv = os.path.join("output", "tabela_rol_procedimentos.csv")
+
+with open(caminho_csv, mode="w", newline="", encoding="utf-8") as arquivo_csv:
+    escritor = csv.writer(arquivo_csv)
+
+    escritor.writerow(["Código", "Descrição", "Tipo", "OutraColuna1", "OutraColuna2"])
+
+    escritor.writerows(todas_linhas)
+
+print(f"CSV salvo em {caminho_csv}")
