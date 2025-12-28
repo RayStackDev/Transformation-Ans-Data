0d55354af20d27323da96a35cf871e274609af64
RayStackDev <raymondlugo64@gmail.com>
2025-12-27 20:22:04 -0400

    Adicionando funçao para limpar expaços extras e quebras de linha.

--- extracao_tabela.py
+++ extracao_tabela.py
@@ -19,39 +19,12 @@ DADOS = {
     'CAPÍTULO': str
 }
 
-
-caminho_pdf = "Anexo/Anexo_I_Rol_2021RN_465.2021_RN654.2025.pdf"
-
-with pdfplumber.open(caminho_pdf) as pdf:
-    print(f"O PDF tem {len(pdf.pages)} paginas.")
-    
-    todas_linhas = []
-
-    for numero_pagina, pagina in enumerate(pdf.pages, start=1):
-
-        tabelas = pagina.extract_tables()
-    
-
-        for tabela in tabelas:
-            for linha in tabela[1:]:
-                todas_linhas.append(linha)
-
-substituicoes = {
-    "OD": "Odontologica",
-    "AMD": "Ambulatorial"
+Mapeamento_legenda = {
+    'OD': 'Seg. Odontológica',
+    'AMB': 'Seg. Ambulatorial'
 }
 
-for linha in todas_linhas:
-    if linha[2] in substituicoes:
-        linha[2] = substituicoes[linha[2]]
-
-caminho_csv = os.path.join("output", "tabela_rol_procedimentos.csv")
-
-with open(caminho_csv, mode="w", newline="", encoding="utf-8") as arquivo_csv:
-    escritor = csv.writer(arquivo_csv)
-
-    escritor.writerow(["Código", "Descrição", "Tipo", "OutraColuna1", "OutraColuna2"])
-
-    escritor.writerows(todas_linhas)
-
-print(f"CSV salvo em {caminho_csv}")
+def limpar_texto(texto):
+    if pd.isna(texto):
+        return pd.NA
+    return " ". join(str(texto).strip().replace("\n"))
